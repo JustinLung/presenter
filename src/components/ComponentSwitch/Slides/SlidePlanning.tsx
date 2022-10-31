4509b9fa007fae3400c00d346eb5d5903ae5d993
DaanKorver <daank2000@hotmail.nl>
2022-10-31 11:39:18 +0100

    Added slide image and removed planning slide

--- src/components/ComponentSwitch/Slides/SlidePlanning.tsx
+++
@@ -1,9 +0,0 @@
-import { Box } from '@chakra-ui/react'
-
-export function SlidePlanning() {
-	return <Box>SlidePlanning</Box>
-}
-
-export function SlidePlanningThumbnail() {
-	return <Box>SlidePlanningThumb</Box>
-}
